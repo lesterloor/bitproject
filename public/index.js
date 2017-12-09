04badaecb477d6c4afbf121098e936babdc13629
lesterloor <lester@theblumarket.com>
2017-12-08 21:42:54 -0500

    added charts with crypto stats

--- public/index.js
+++ public/index.js
@@ -1,45 +1,30 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    var ctx = document.getElementById("myChart");
     console.log($("#usdPrice").text());
-    const USD = $("#usdPrice").text();
-    const EUR = $("#eurPrice").text();
+    const BTC = $("#btcPrice").text();
+    const LTC = $("#ltcPrice").text();
     const ETH = $("#ethPrice").text();
-  var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: ["USD", "ETH", "EUR"],
-          datasets: [{
-              label: '# of Votes',
-              data: [USD, ETH, EUR],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }]
+    const XRP = $("#xrpPrice").text();
+    new Chart(document.getElementById("doughnut-chart"), {
+        type: 'doughnut',
+        data: {
+          labels: ["BTC", "ETH", "LTC", "XRP"],
+          datasets: [
+            {
+              label: "Assets Allocation",
+              backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","red"],
+              data: [BTC,ETH,LTC,XRP]
+            }
+          ]
+        },
+        options: {
+          title: {
+            display: true,
+            text: 'Predicted world population (millions) in 2050'
           }
-      }
-  });
+        }
+    });
+
+
 
 });
